28696058528d7c98782ceb848d5bda3f34e6026f
guillaume.caya <guillaume.caya@gsoft.com>
2024-07-30 18:30:28 -0400

    Improvements

--- tests/SystemTests.cs
+++ tests/SystemTests.cs
@@ -1,28 +1,14 @@
-using System.Text;
-using CliWrap;
-using Markdig;
-using Markdig.Extensions.Tables;
 using Markdig.Renderers.Normalize;
 using Markdig.Syntax;
 using Markdig.Syntax.Inlines;
-using Meziantou.Framework;
 using Meziantou.Framework.InlineSnapshotTesting;
-using Octokit;
-using Octokit.Internal;
 using Xunit.Abstractions;
 
 namespace renovate_config.tests;
 
 public class SystemTests
 {
-    private const string GitUrl = "https://github.com/gsoft-inc/renovate-config-test";
-
-    private const string Githubpat = ""; // GH auth token
-
     private readonly ITestOutputHelper _testOutputHelper;
-    private readonly string _uniqueId = $"{DateTime.UtcNow:yyyyMMdd_HHmmss}_{Guid.NewGuid().ToString("N").ToLowerInvariant()}";
-
-    private readonly InMemoryCredentialStore credentials = new InMemoryCredentialStore(new Octokit.Credentials(Githubpat));
 
     public SystemTests(ITestOutputHelper testOutputHelper)
     {
@@ -32,65 +18,16 @@ public class SystemTests
     [Fact]
     public async Task RenovateDotnetSdkDependencies()
     {
-        await using var temporaryDirectory = TemporaryDirectory.Create();
-        var repoPath = temporaryDirectory.FullPath;
-        var branchName = "main";
-        await this.ExecuteCommand("git", ["clone", GitUrl, repoPath]);
-        await this.ExecuteCommand("git", ["-C", repoPath, "rm", "-rf", "."]);
 
-        this.CopyRenovateFile(temporaryDirectory);
-        temporaryDirectory.CreateTextFile("global.json", /*lang=json*/"""{"sdk": {"version": "5.0.100"}}""");
+        var testContext = await TestContext.CreateAsync(_testOutputHelper);
 
-        await this.ExecuteCommand("git", ["-C", repoPath, "add", "."] );
-        await this.ExecuteCommand("git", ["-C", repoPath, "commit", "--message", "IDP ScaffoldIt automated test", "--author", "IDP ScaffoldIt <idp@workleap.com>"]);
-        await this.ExecuteCommand("git", ["-C", repoPath, "push", "--set-upstream", GitUrl, branchName]);
+        testContext.AddFile("global.json", /*lang=json*/"""{"sdk": {"version": "5.0.100"}}""");
 
-        await this.ExecuteCommand(
-            "docker",
-            [
-                "run",
-                "--rm",
-                "-e", "LOG_LEVEL=debug",
-                "-e", "RENOVATE_PRINT_CONFIG=true",
-                "-e", $"RENOVATE_TOKEN={Githubpat}",
-                "-e", "RENOVATE_RECREATE_WHEN=always",
-                "-e", "RENOVATE_INHERIT_CONFIG_FILE_NAME=not-renovate.json",
-                "-e", "RENOVATE_REPOSITORIES=[\"https://github.com/gsoft-inc/renovate-config-test\"]",
-                "renovate/renovate:latest",
-                "renovate",
-                "gsoft-inc/renovate-config-test"
-            ]);
+        await testContext.RunRenovate();
 
-        var githubClient = new GitHubClient(new ProductHeaderValue("renovate-test"), credentials);
-        var pullRequests = await githubClient.PullRequest.GetAllForRepository("gsoft-inc", "renovate-config-test", new PullRequestRequest(){ Base = branchName});
+        var pullRequests = await testContext.GetPullRequests();
 
-        var pipeline = new MarkdownPipelineBuilder().UseAdvancedExtensions().Build();
-
-        var pullRequestsInfos = new List<PullRequestInfos>(pullRequests.Count);
-
-        foreach (var pullRequest in pullRequests.OrderBy(x => x.Title))
-        {
-            MarkdownDocument markdownDocument = Markdown.Parse(pullRequest.Body, pipeline);
-            var prTitle = pullRequest.Title;
-
-            var table = markdownDocument.OfType<Table>().First();
-            var rows = table.Skip(1).OfType<TableRow>().ToArray();
-
-            var packageUpdateInfos = new List<PackageUpdateInfos>(rows.Length);
-
-            foreach (var row in rows)
-            {
-                var packge = row[0].InnerText().Trim();
-                var type = row[1].InnerText().Trim();
-                var update = row[2].InnerText().Trim();
-
-                packageUpdateInfos.Add(new PackageUpdateInfos(packge, type, update));
-            }
-
-            pullRequestsInfos.Add(new PullRequestInfos(prTitle, packageUpdateInfos.OrderBy(x => x.Package).ThenBy(x => x.Type).ThenBy(x => x.Update)));
-        }
-
-        InlineSnapshot.Validate(pullRequestsInfos.OrderBy(x => x.Title), """
+        InlineSnapshot.Validate(pullRequests, """
             - Title: chore(deps): update dependency dotnet-sdk to v5.0.408
               PackageUpdatesInfos:
                 - Package: dotnet-sdk
@@ -102,42 +39,6 @@ public class SystemTests
                   Type: dotnet-sdk
                   Update: major
             """);
-    }
-
-    private void CopyRenovateFile(TemporaryDirectory temporaryDirectory)
-    {
-        var gitRoot = GetGitRoot();
-
-        File.Copy(gitRoot / "default.json", temporaryDirectory.FullPath / "renovate.json");
-    }
-
-    private async Task<(string StdOut, string StdError)> ExecuteCommand(string executable, string[] args, KeyValuePair<string, string>[]? envVariables = null)
-    {
-        var stdOut = new StringBuilder();
-        var stdErr = new StringBuilder();
-        await Cli.Wrap(executable).WithArguments(args)
-            .WithEnvironmentVariables(builder =>
-            {
-                foreach (var (key, value) in envVariables ?? [])
-                {
-                    builder.Set(key, value);
-                }
-            })
-            .WithStandardOutputPipe(PipeTarget.Merge(PipeTarget.ToStringBuilder(stdOut), PipeTarget.ToDelegate(_testOutputHelper.WriteLine)))
-            .WithStandardErrorPipe(PipeTarget.Merge(PipeTarget.ToStringBuilder(stdErr), PipeTarget.ToDelegate(_testOutputHelper.WriteLine)))
-            .ExecuteAsync();
-
-        return (stdOut.ToString(), stdErr.ToString());
-    }
-
-    private static FullPath GetGitRoot()
-    {
-        if (FullPath.CurrentDirectory().TryFindFirstAncestorOrSelf(current => Directory.Exists(current / ".git"), out var root))
-        {
-            return root;
-        }
-
-        throw new InvalidOperationException("root folder not found");
     }
 }
 
---
+++ tests/TestContext.cs
@@ -0,0 +1,174 @@
+using System.Text;
+using CliWrap;
+using Markdig;
+using Markdig.Extensions.Tables;
+using Markdig.Syntax;
+using Meziantou.Framework;
+using Octokit;
+using Octokit.Internal;
+using Xunit.Abstractions;
+
+namespace renovate_config.tests;
+
+public class TestContext(ITestOutputHelper outputHelper, TemporaryDirectory temporaryDirectory, GitHubClient gitHubClient)
+{
+    private const string DefaultBranchName = "main";
+    private const string GitUrl = "https://github.com/gsoft-inc/renovate-config-test";
+    private readonly string _repoPath = temporaryDirectory.FullPath;
+
+    public static async Task<TestContext> CreateAsync(ITestOutputHelper outputHelper)
+    {
+        var gitGubClient = await CreateGitHubClient(outputHelper);
+
+        var temporaryDirectory = TemporaryDirectory.Create();
+        var repoPath = temporaryDirectory.FullPath;
+        await ExecuteCommand(outputHelper, "git", ["-C", repoPath, "init", "--initial-branch=main"]);
+
+        CopyRenovateFile(temporaryDirectory);
+
+        return new TestContext(outputHelper, temporaryDirectory, gitGubClient);
+    }
+
+    public void AddFile(string path, string content)
+    {
+        temporaryDirectory.CreateTextFile(path, content);
+    }
+
+    public async Task RunRenovate()
+    {
+        var token = await GetGitHubToken(outputHelper);
+
+        await this.CleanupRepository();
+
+        await ExecuteCommand(outputHelper, "git", ["-C", _repoPath, "add", "."] );
+        await ExecuteCommand(outputHelper, "git", ["-C", _repoPath, "commit", "--message", "IDP ScaffoldIt automated test", "--author", "IDP ScaffoldIt <idp@workleap.com>"]);
+        await ExecuteCommand(outputHelper, "git", ["-C", _repoPath, "push", GitUrl, DefaultBranchName + ":" + DefaultBranchName, "--force"]);
+
+        await ExecuteCommand(
+            outputHelper,
+            "docker",
+            [
+                "run",
+                "--rm",
+                "-e", "LOG_LEVEL=debug",
+                "-e", "RENOVATE_PRINT_CONFIG=true",
+                "-e", $"RENOVATE_TOKEN={token}",
+                "-e", "RENOVATE_RECREATE_WHEN=always",
+                "-e", "RENOVATE_INHERIT_CONFIG_FILE_NAME=not-renovate.json",
+                "-e", "RENOVATE_REPOSITORIES=[\"https://github.com/gsoft-inc/renovate-config-test\"]",
+                "renovate/renovate:latest",
+                "renovate",
+                "gsoft-inc/renovate-config-test"
+            ]);
+    }
+
+    public async Task<IEnumerable<PullRequestInfos>> GetPullRequests()
+    {
+        var pullRequests = await gitHubClient.PullRequest.GetAllForRepository("gsoft-inc", "renovate-config-test", new PullRequestRequest(){ Base = DefaultBranchName});
+
+        var pipeline = new MarkdownPipelineBuilder().UseAdvancedExtensions().Build();
+
+        var pullRequestsInfos = new List<PullRequestInfos>(pullRequests.Count);
+
+        foreach (var pullRequest in pullRequests.OrderBy(x => x.Title))
+        {
+            MarkdownDocument markdownDocument = Markdown.Parse(pullRequest.Body, pipeline);
+            var prTitle = pullRequest.Title;
+
+            var table = markdownDocument.OfType<Table>().First();
+            var rows = table.Skip(1).OfType<TableRow>().ToArray();
+
+            var packageUpdateInfos = new List<PackageUpdateInfos>(rows.Length);
+
+            foreach (var row in rows)
+            {
+                var packge = row[0].InnerText().Trim();
+                var type = row[1].InnerText().Trim();
+                var update = row[2].InnerText().Trim();
+
+                packageUpdateInfos.Add(new PackageUpdateInfos(packge, type, update));
+            }
+
+            pullRequestsInfos.Add(new PullRequestInfos(prTitle, packageUpdateInfos.OrderBy(x => x.Package).ThenBy(x => x.Type).ThenBy(x => x.Update)));
+        }
+
+        return pullRequestsInfos;
+    }
+
+    private async Task CleanupRepository()
+    {
+        var branches = await gitHubClient.Repository.Branch.GetAll("gsoft-inc", "renovate-config-test");
+        var branchesToDelete = branches.Where(x => x.Name != DefaultBranchName);
+
+        foreach (var branch in branchesToDelete)
+        {
+            await gitHubClient.Git.Reference.Delete("gsoft-inc", "renovate-config-test", $"heads/{branch.Name}");
+        }
+    }
+
+    private static async Task<string> GetGitHubToken(ITestOutputHelper outputHelper)
+    {
+        var token = Environment.GetEnvironmentVariable("TEST_GITHUB_TOKEN");
+
+        if (string.IsNullOrEmpty(token))
+        {
+            var (stdout, _) = await ExecuteCommand(outputHelper, "gh", ["auth", "token"]);
+
+            token = stdout.Trim();
+        }
+
+        if (string.IsNullOrEmpty(token))
+        {
+            throw new Exception("GitHub token not found, run `gh auth login` to authenticate");
+        }
+
+        return token;
+    }
+
+    private static async Task<GitHubClient> CreateGitHubClient(ITestOutputHelper outputHelper)
+    {
+        var token = await GetGitHubToken(outputHelper);
+
+        var githubClient = new GitHubClient(new ProductHeaderValue("renovate-test"), new InMemoryCredentialStore(new Octokit.Credentials(token)));
+
+        return githubClient;
+    }
+
+
+    private static void CopyRenovateFile(TemporaryDirectory temporaryDirectory)
+    {
+        var gitRoot = GetGitRoot();
+
+        File.Copy(gitRoot / "default.json", temporaryDirectory.FullPath / "renovate.json");
+    }
+
+    private static FullPath GetGitRoot()
+    {
+        if (FullPath.CurrentDirectory().TryFindFirstAncestorOrSelf(current => Directory.Exists(current / ".git"), out var root))
+        {
+            return root;
+        }
+
+        throw new InvalidOperationException("root folder not found");
+    }
+
+    private static async Task<(string StdOut, string StdError)> ExecuteCommand(ITestOutputHelper outputHelper,
+        string executable, string[] args, KeyValuePair<string, string>[]? envVariables = null)
+    {
+        var stdOut = new StringBuilder();
+        var stdErr = new StringBuilder();
+        await Cli.Wrap(executable).WithArguments(args)
+            .WithEnvironmentVariables(builder =>
+            {
+                foreach (var (key, value) in envVariables ?? [])
+                {
+                    builder.Set(key, value);
+                }
+            })
+            .WithStandardOutputPipe(PipeTarget.Merge(PipeTarget.ToStringBuilder(stdOut), PipeTarget.ToDelegate(outputHelper.WriteLine)))
+            .WithStandardErrorPipe(PipeTarget.Merge(PipeTarget.ToStringBuilder(stdErr), PipeTarget.ToDelegate(outputHelper.WriteLine)))
+            .ExecuteAsync();
+
+        return (stdOut.ToString(), stdErr.ToString());
+    }
+}
