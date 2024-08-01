370dba55aa226915cf70e970ff5ded0d8b6bc15a
Guillaume Caya-Letourneau <guillaume.caya.letourneau@gmail.com>
2024-08-01 11:58:47 -0400

    Idp-1922 - Add system tests to default.json (#2)
    Co-authored-by: Gérald Barré <meziantou@users.noreply.github.com>

---
+++ tests/MarkdownExtensions.cs
@@ -0,0 +1,21 @@
+using Markdig.Renderers.Normalize;
+using Markdig.Syntax;
+using Markdig.Syntax.Inlines;
+
+namespace renovate_config.tests;
+
+internal static class MarkdownExtensions
+{
+    public static string ToNormalizedString(this MarkdownObject obj)
+    {
+        using var writer = new StringWriter();
+        var renderer = new NormalizeRenderer(writer);
+        renderer.Render(obj);
+        return writer.ToString();
+    }
+    public static string InnerText(this MarkdownObject obj)
+    {
+        var inlines = obj.Descendants<LiteralInline>();
+        return string.Join(" ", inlines.Select(inline=> inline.ToNormalizedString()));
+    }
+}
---
+++ tests/SystemTests.cs
@@ -0,0 +1,193 @@
+using Xunit.Abstractions;
+
+namespace renovate_config.tests;
+
+public sealed class SystemTests(ITestOutputHelper testOutputHelper)
+{
+    [Fact]
+    public async Task RenovateDotnetSdkDependencies()
+    {
+        var testContext = await TestContext.CreateAsync(testOutputHelper);
+
+        testContext.AddFile("global.json", /*lang=json*/"""{"sdk": {"version": "6.0.100"}}""");
+        testContext.AddFile("Dockerfile",
+            """
+            FROM mcr.microsoft.com/dotnet/sdk:6.0.100
+            FROM mcr.microsoft.com/dotnet/aspnet:6.0.0
+            """);
+
+        await testContext.RunRenovate();
+
+        await testContext.AssertPullRequests(
+            """
+            - Title: chore(deps): update dotnet-sdk
+              Labels:
+                - renovate
+              PackageUpdatesInfos:
+                - Package: dotnet-sdk
+                  Type: dotnet-sdk
+                  Update: patch
+                - Package: mcr.microsoft.com/dotnet/aspnet
+                  Type: final
+                  Update: patch
+                - Package: mcr.microsoft.com/dotnet/sdk
+                  Type: stage
+                  Update: patch
+            - Title: chore(deps): update dotnet-sdk  to redacted(major)
+              Labels:
+                - renovate
+              PackageUpdatesInfos:
+                - Package: dotnet-sdk
+                  Type: dotnet-sdk
+                  Update: major
+                - Package: mcr.microsoft.com/dotnet/aspnet
+                  Type: final
+                  Update: major
+                - Package: mcr.microsoft.com/dotnet/sdk
+                  Type: stage
+                  Update: major
+            """);
+    }
+
+    [Fact]
+    public async Task RenovateHangfireDependencies()
+    {
+      var testContext = await TestContext.CreateAsync(testOutputHelper);
+
+      testContext.AddFile("project.csproj",
+        """
+        <Project Sdk="Microsoft.NET.Sdk">
+          <ItemGroup>
+            <PackageReference Include="Hangfire" Version="1.7.0" />
+            <PackageReference Include="Hangfire.Core" Version="1.7.0" />
+            <PackageReference Include="Hangfire.NetCore" Version="1.7.0" />
+            <PackageReference Include="Hangfire.Pro" Version="2.3.2" />
+            <PackageReference Include="Hangfire.Pro.Redis" Version="2.3.2" />
+            <PackageReference Include="Hangfire.Throttling" Version="1.1.1" />
+          </ItemGroup>
+        </Project>
+        """);
+
+      await testContext.RunRenovate();
+
+      await testContext.AssertPullRequests(
+        """
+        - Title: chore(deps): update dependency hangfire  to redacted
+          Labels:
+            - renovate
+          PackageUpdatesInfos:
+            - Package: Hangfire
+              Type: nuget
+              Update: minor
+        - Title: chore(deps): update hangfire
+          Labels:
+            - renovate
+          PackageUpdatesInfos:
+            - Package: Hangfire.Core
+              Type: nuget
+              Update: minor
+            - Package: Hangfire.NetCore
+              Type: nuget
+              Update: minor
+            - Package: Hangfire.Pro.Redis
+              Type: nuget
+              Update: minor
+        """);
+    }
+
+    [Fact]
+    public async Task RenovatePackageJsonDependencies()
+    {
+      var testContext = await TestContext.CreateAsync(testOutputHelper);
+
+      testContext.AddFile("package.json",
+        """
+        {
+          "dependencies": {
+            "@azure/msal-browser": "^3.13.0",
+            "@azure/msal-react": "~2.0.15"
+          },
+          "devDependencies": {
+            "@storybook/addon-essentials": "^8.0.10",
+            "@storybook/addon-interactions": "~8.0.10"
+          }
+        }
+        """);
+
+      await testContext.RunRenovate();
+
+      await testContext.AssertPullRequests(
+          """
+        - Title: fix(deps): pin dependencies
+          Labels:
+            - renovate
+          PackageUpdatesInfos:
+            - Package: @azure/msal-browser
+              Type: dependencies
+              Update: pin
+            - Package: @azure/msal-react
+              Type: dependencies
+              Update: pin
+            - Package: @storybook/addon-essentials
+              Type: devDependencies
+              Update: pin
+            - Package: @storybook/addon-interactions
+              Type: devDependencies
+              Update: pin
+        """);
+    }
+
+    [Fact]
+    public async Task RenovateMicrosoftDependencies()
+    {
+      var testContext = await TestContext.CreateAsync(testOutputHelper);
+
+      testContext.AddFile("project.csproj",
+        """
+        <Project Sdk="Microsoft.NET.Sdk">
+          <ItemGroup>
+            <PackageReference Include="System.Text.Json" Version="7.0.0" />
+            <PackageReference Include="Microsoft.ApplicationInsights.AspNetCore" Version="1.0.2" />
+            <PackageReference Include="microsoft.AspNetCore.Authentication.OpenIdConnect" Version="7.0.0" />
+            <PackageReference Include="Microsoft.Azure.AppConfiguration.AspNetCore" Version="7.0.0" />
+            <PackageReference Include="Microsoft.CodeAnalysis.PublicApiAnalyzers" Version="3.3.4">
+              <PrivateAssets>all</PrivateAssets>
+              <IncludeAssets>runtime; build; native; contentfiles; analyzers; buildtransitive</IncludeAssets>
+            </PackageReference>
+          </ItemGroup>
+        </Project>
+        """);
+
+      await testContext.RunRenovate();
+
+      await testContext.AssertPullRequests(
+          """
+        - Title: chore(deps): update microsoft
+          Labels:
+            - renovate
+          PackageUpdatesInfos:
+            - Package: microsoft.AspNetCore.Authentication.OpenIdConnect
+              Type: nuget
+              Update: patch
+            - Package: Microsoft.Azure.AppConfiguration.AspNetCore
+              Type: nuget
+              Update: minor
+            - Package: System.Text.Json
+              Type: nuget
+              Update: patch
+        - Title: chore(deps): update microsoft (major)
+          Labels:
+            - renovate
+          PackageUpdatesInfos:
+            - Package: Microsoft.ApplicationInsights.AspNetCore
+              Type: nuget
+              Update: major
+            - Package: microsoft.AspNetCore.Authentication.OpenIdConnect
+              Type: nuget
+              Update: major
+            - Package: System.Text.Json
+              Type: nuget
+              Update: major
+        """);
+    }
+}
