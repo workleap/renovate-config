7127f3e79c0acfad97f6cef26f9ef27599a0827c
Anthony Simmon <anthony.simmon@workleap.com>
2025-04-03 16:36:10 -0400

    Added temporary branch name models

---
+++ tests/TemporaryBranchName.cs
@@ -0,0 +1,23 @@
+namespace renovate_config.tests;
+
+internal abstract class TemporaryBranchName
+{
+    public const string DateFormat = "yyyyMMddHHmmss";
+    public static readonly TimeSpan MaximumAge = TimeSpan.FromDays(3);
+    protected const char Slash = '/';
+    protected const char Underscore = '_';
+
+    public abstract DateTimeOffset Date { get; }
+
+    public abstract string Id { get; }
+
+    public bool HasExpired()
+    {
+        return this.HasExpired(DateTimeOffset.UtcNow);
+    }
+
+    public bool HasExpired(DateTimeOffset utcNow)
+    {
+        return this.Date <= utcNow.Add(MaximumAge.Negate());
+    }
+}
---
+++ tests/TemporaryFeatureBranchName.cs
@@ -0,0 +1,50 @@
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+
+namespace renovate_config.tests;
+
+internal sealed class TemporaryFeatureBranchName : TemporaryBranchName
+{
+    private const string FolderName = "feature";
+
+    public TemporaryFeatureBranchName()
+        : this(DateTimeOffset.UtcNow, Guid.NewGuid().ToString("N"))
+    {
+    }
+
+    private TemporaryFeatureBranchName(DateTimeOffset date, string id)
+    {
+        this.Date = date;
+        this.Id = id;
+        this.Name = $"{FolderName}{Slash}{date.ToString(DateFormat, CultureInfo.InvariantCulture)}{Underscore}{id}";
+    }
+
+    public override DateTimeOffset Date { get; }
+
+    public override string Id { get; }
+
+    public string Name { get; }
+
+    public static bool TryParse(string branchName, [NotNullWhen(true)] out TemporaryFeatureBranchName? result)
+    {
+        if (branchName.Split(Slash, Underscore) is not [FolderName, { } datePart, { } idPart])
+        {
+            result = null;
+            return false;
+        }
+
+        if (!DateTimeOffset.TryParseExact(datePart, DateFormat, CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal, out var date))
+        {
+            result = null;
+            return false;
+        }
+
+        result = new TemporaryFeatureBranchName(date, idPart);
+        return true;
+    }
+
+    public override string ToString()
+    {
+        return this.Name;
+    }
+}
